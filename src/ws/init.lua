52e8f4b10e8fcbdcba0d203216e815c397cf9365
KXMN <tech@kaxamana.com.br>
2019-08-21 02:39:17 -0300

    Improved webserver documenting

--- src/ws/init.lua
+++ src/ws/init.lua
@@ -3,14 +3,55 @@ local ws = require('aurora.ws.pegasus');
 --[[md:
 ## aurora.ws - WebServer utility
 
-]]
-
-
-
---[[md
-### ws.new(conf) : Pegasus
-* conf: table with configuration
+### ws.new(ConfTable) : Pegasus
+* ConfTable: table with configuration
 * returns a Pegasus like webserver instance
+
+### ConfTable
+```Lua
+{
+	port: 8080,
+	location: PATH..'/www',
+	plugins: { require('aurora.ws.pegasus.compress'):new},
+	rules: RulesTable
+}
+```
+
+### RulesTable
+```
+{
+	{ '/api', 'module' },
+	{ MATCH, MODULE }
+}
+```
+* MATCH - Lua expression that make processing stop and call MODULE
+* MODULE - A lua module that returns a function
+
+### Rule module file
+The MODULE file looks like:
+```
+return function( request, response, match ) end
+```
+Where:
+* request: request info table
+* response: table with methods that handle output to client
+* match: what was matched on the rule
+
+#### request
+* `:path()` - requested path
+* `:headers()` - headers data table
+* `:methods()` - GET, POST, etc...
+* `:querystring()` - get parameters
+* `:post()` - post parameters
+
+#### response
+* `:addHeader(key, value)` - add a response headers
+* `:addHeaders(table)` - table with addHeader values
+* `:statusCode(numer, string)` - returns status code and message
+* `:contentType(string)` - mime of response
+* `:write(responseBodyString)` - write back to client
+* `:writeFile(filename)` - ?
+
 ]]
 return {
 	new = function(conf)
