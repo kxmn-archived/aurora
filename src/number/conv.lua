a7701f19fd766d1187b7589ef9cf5247035d0798
KXMN <tech@kaxamana.com.br>
2019-08-31 03:06:57 -0300

    Added number library with conversion and random methods

---
+++ src/number/conv.lua
@@ -0,0 +1,99 @@
+local M = {}
+
+--[[md:
+### number.base.toValues(base,value)
+
+* base : base used to convert value
+* value : a number
+* Returns a table with each result digit at one position;
+
+Example:
+```
+local digits = aurora.number.toValues(16,4095))
+print(table.concat(digits,','))
+= "15,15,15"
+```
+]]
+function M.toValues (b,v)
+	local l={}
+	while v > 0 do
+		table.insert(l,1,v%b)
+		v = v//b
+	end
+	return l
+end
+
+
+--[[md:
+### number.base.fromValues(base,value) : number
+
+* base : base number that represents previous used to encode
+* value : a table where each digit is in one position;
+* Returns a number
+
+Example:
+```
+print(aurora.number.fromValues({15,15,15},16))
+= "4095"
+```
+]]
+function M.fromValues(b,l)
+	local v=0
+
+	for _,d in pairs(l) do
+		v = b * v + d
+	end
+	return v
+end
+
+local Digits = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+
+--[[md:
+### number.toBase(base,decimal)
+
+* base : target base for conversion (maximum 62)
+* decimal : integer number to be converted
+* Returns a string with converted data
+
+The base is a integer until maximum of 62. The order is 0-9,a-z,A-Z.
+if you use in a case insensitive context, prefer use base 36 instead 62
+
+Example:
+```
+print(aurora.number.tobase(16,4095))
+= "fff"
+```
+
+]]
+function M.toBase(b,d)
+	local l = M.toValues(b,d)
+	local li=0
+	for i=1, #l, 1 do li=l[i]+1 l[i]=Digits:sub(li,li) end
+	return table.concat(l,'')
+end
+
+--[[md:
+### number.fromBase(base,string)
+
+* base : target base for conversion (maximum 62) (see toBase for info)
+* string : string representation that should be converted back to decimal
+* Returns a number with converted data
+
+Example:
+```
+print(aurora.number.fromBase(16,'fff'))
+= 4095
+```
+]]
+function M.fromBase(b,d)
+	local li={}
+	for i=1, #d, 1 do li[#li+1]=Digits:find(d:sub(i,i))-1 end
+	return M.fromValues(b,li)
+end
+
+
+
+local res = M.toBase(16,4095)
+print(res)
+print(M.fromBase(16,res))
+print(M.fromBase(16,'ffffff'))
---
+++ src/number/init.lua
@@ -0,0 +1,32 @@
+--[[md:
+## aurora.number - number handling utils
+
+]]
+
+
+local M=ondemand('aurora.number')
+
+
+--[[md:
+### aurora.numer.rand(bytes,mask)
+
+* bytes : (default=4) bytes that should be read from /dev/urandom
+* mask : (default=256) mask used incrementally for each
+* Returns a random number
+
+]]
+
+function M.rand(b,m)
+	b=b or 4
+	local f = io.open ('/dev/urandom','rb')
+	local n, input = 0, f:read (b)
+	for i=1, input:len () do
+		n = 256 * n + input:byte (i)
+	end
+	f:close()
+	return n
+end
+
+
+
+return M
