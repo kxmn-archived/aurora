1b86a8b46195599a88103076bae0c2ed8e35c96d
KXMN <tech@kaxamana.com.br>
2019-08-31 15:30:27 -0300

    Table handling utils

---
+++ src/table/init.lua
@@ -0,0 +1,21 @@
+--[[md:
+## aurora.table - utils for handling tables
+
+]]
+local M = ondemand('aurora.table')
+
+
+--[[md:
+### aurora.table.toSet (list)
+
+* list : a list table
+* returns a table indexed with values of list, easily searchable
+]]
+function M.toSet (list)
+	local set = {}
+	for _,v in ipairs(list) do set[v]=true end
+	return set
+end
+
+
+return M
