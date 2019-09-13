f3f91d86e2fa116517a5ab6e839afe4ac96802ed
KXMN <tech@kaxamana.com.br>
2019-09-13 01:53:13 -0300

    Documentation correction

--- src/fs/mkdir.lua
+++ src/fs/mkdir.lua
@@ -3,24 +3,26 @@ local lfs = require('lfs')
 local fs = aurora.fs
 
 --[[md:
-fs.mkdir(path)
+### fs.mkdir(path)
 
 * path : string of the path to be created
 * returns true, nil on success or false, message on error
 
 Observe that this function create recursively dirs
 ]]
-return function (p)
-	if fs.isDir(p) then return true end
+return function (path)
+	local cat = table.concat
+	if fs.isDir(path) then return true end
 
-	local p = split(p,'/')
-	local s = ''
+	local p = split(path,'/')
+	local s = {}
 	local m, me;
 	for i,d in ipairs(p) do
-		s = s..d..'/'
-		m, me = lfs.mkdir(s)
+		s[i * 2 - 1] = d
+		s[i * 2] = '/'
+		m, me = lfs.mkdir(cat(s))
 	end
-	if fs.isDir(p) then
+	if fs.isDir(path) then
 		return true,nil
 	else
 		return m,me
